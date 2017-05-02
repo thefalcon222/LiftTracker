16cbc332e5bb9bc0605a1f1e9e41131252cb6ea0
Andrew Walters <ajw222@vt.edu>
2017-05-02 15:19:56 -0400

    Added code for options menu in MainActivity

--- app/src/main/java/com/example/ryan/lifttracker/MainActivity.java
+++ app/src/main/java/com/example/ryan/lifttracker/MainActivity.java
@@ -1,20 +1,61 @@
 package com.example.ryan.lifttracker;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.ryan.lifttracker.fragments.WorkoutFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         //RYAN WAS HERE!!
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_home) {
+
+        }
+        else if (id == R.id.action_set_alert) {
+
+        }
+
+        else if (id == R.id.action_logout) {
+            // Log the user out
+            prefs.edit().remove("status").apply();
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
