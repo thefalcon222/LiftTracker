4249c5ca676864a7da4f5cc61c07d62e1a0f9841
Andrew Walters <ajw222@vt.edu>
2017-05-02 18:46:02 -0400

    Added createEmail.java Very simple Android email client.  Needs implementation inside actual codebase with "startActivity(intent...)" in MainActivity.

---
+++ app/src/main/java/com/example/ryan/lifttracker/email/createEmail.java
@@ -0,0 +1,35 @@
+package com.example.ryan.lifttracker.email;
+
+import android.content.Context;
+import android.content.Intent;
+import static android.R.id.message;
+
+/**
+ * Created by Andrew Walters on 5/2/2017.
+ */
+
+public class createEmail
+{
+
+    Intent chooser;
+
+    public createEmail(String to, String subject)
+    {
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+        email.putExtra(Intent.EXTRA_SUBJECT, subject);
+        email.putExtra(Intent.EXTRA_TEXT, message);
+
+        email.setType("message/rfc822");
+        chooser = Intent.createChooser(email, "Send mail...");
+
+        //Then we need to actually start the activity
+    }
+
+    public Intent getChooser()
+    {
+        return chooser;
+    }
+}
+
+
