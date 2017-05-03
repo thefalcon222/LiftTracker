8c903858c83eb8ccc151c0a505c3557ebc79f5f5
ryans5 <ryans5@vt.edu>
2017-05-03 18:35:04 -0400

    added code for addWorkoutFragment

--- app/src/main/java/com/example/ryan/lifttracker/fragments/AddWorkoutFragment.java
+++ app/src/main/java/com/example/ryan/lifttracker/fragments/AddWorkoutFragment.java
@@ -1,13 +1,18 @@
 package com.example.ryan.lifttracker.fragments;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ryan.lifttracker.R;
 
@@ -19,6 +24,11 @@ import static com.example.ryan.lifttracker.R.layout.add_workout;
 
 public class AddWorkoutFragment extends Fragment implements View.OnClickListener{
     public static final String TAG_ADD_WORKOUT_FRAGMENT = "add_workout_fragment";
+    private Button addEx, submit, subtractSet,addSet;
+    private LinearLayout fillEX;
+    private EditText workoutName, workoutDescription;
+    private TextView workoutReps;
+    private int reps;
 
     public static AddWorkoutFragment newInstance() {
         AddWorkoutFragment fragment = new AddWorkoutFragment();
@@ -45,40 +55,58 @@ public class AddWorkoutFragment extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.add_workout, container, false);
-        Button addEx = (Button) v.findViewById(R.id.addEButton);
-        Button submit = (Button) v.findViewById(R.id.submitWButton);
-        Button addSet = (Button) v.findViewById(R.id.addSet);
-        Button subtractSet = (Button) v.findViewById(R.id.subtractSet);
+        addEx = (Button) v.findViewById(R.id.addEButton);
+        submit = (Button) v.findViewById(R.id.submitWButton);
+        //addSet = (Button) v.findViewById(R.id.addSet);
+        //subtractSet = (Button) v.findViewById(R.id.subtractSet);
+        fillEX = (LinearLayout) v.findViewById(R.id.exercise_container);
+        workoutName = (EditText) v.findViewById(R.id.woName);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        addEx.setOnClickListener(this);
+        submit.setOnClickListener(this);
+        //addSet.setOnClickListener(this);
+        //subtractSet.setOnClickListener(this);
+
+        return v;
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.addEButton:
-
-                LinearLayout fillEX = (LinearLayout) view.findViewById(R.id.exercise_container);
                 LayoutInflater inflater = (LayoutInflater)view.getContext().getSystemService
                         (view.getContext().LAYOUT_INFLATER_SERVICE);
                 View exFiller = (View) inflater.inflate(R.layout.exercise_details, null);
                 fillEX.addView(exFiller);
-
+                addSet = (Button) exFiller.findViewById(R.id.addSet);
+                subtractSet = (Button) exFiller.findViewById(R.id.subtractSet);
+                addSet.setOnClickListener(this);
+                subtractSet.setOnClickListener(this);
+                workoutDescription = (EditText) exFiller.findViewById(R.id.exName);
+                workoutReps = (TextView) exFiller.findViewById(R.id.repsView);
+                reps = 0;
                 break;
             case R.id.submitWButton:
                 // Something something send to database
+                Calendar c = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    c = Calendar.getInstance();
+                    String date = DateFormat.format("MMMM d, yyyy ", c.getTime()).toString();
+                }
+                String name = workoutName.getText().toString();
+                String description = workoutDescription.getText().toString();
+                String repString = workoutReps.getText().toString();
+
+                //add to database
+
                 break;
             case R.id.addSet:
-
-                LinearLayout fillROWS = (LinearLayout) view.findViewById(R.id.row_container);
-                LayoutInflater rowInflater = (LayoutInflater)view.getContext().getSystemService
-                        (view.getContext().LAYOUT_INFLATER_SERVICE);
-                View rowFiller = rowInflater.inflate(R.layout.exercise_rows, null);
-                fillROWS.addView(rowFiller);
-
+                reps++;
+                workoutReps.setText(reps + " reps");
                 break;
             case R.id.subtractSet:
-                // Something something delete a row
+                reps--;
+                workoutReps.setText(reps + " reps");
                 break;
 
             default:
